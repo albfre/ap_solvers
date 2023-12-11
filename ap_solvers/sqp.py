200bb9d4862b89e735337b09f1e932b9dbded1d8
Albin Fredriksson <albin.fredriksson@gmail.com>
2023-12-11 23:39:09 +0100

    Update SQP

--- ap_solvers/sqp.py
+++ ap_solvers/sqp.py
@@ -7,7 +7,7 @@ class Sqp:
     Initialize the Sqp object with a set of points.
 
     minimize f(x)
-    s.t. c_i(x) <= 0 for all i
+    s.t. c_i(x) >= 0 for all i
 
     if f_grad or c_grad is None, they will be approximated by finite differences
     """
@@ -17,13 +17,13 @@ class Sqp:
     self.c_grads = c_grads if c_grads else []
     self.rho = [mp.zero] * len(self.cs)
     self.tol = tol
-    self.minor_tol = tol
+    self.minor_tol = tol / 100
     self.matrix = matrix
     self.print_stats = True
     self.eta = mp.mpf('0.5')
     self.x_used_for_gradient_computation = None
 
-  def solve(self, x0, max_iter = 20):
+  def solve(self, x0, max_iter = 100):
     n = len(x0)
     self.hessian_approximation = self.matrix(n, n)
     for i in range(n):
@@ -35,7 +35,6 @@ class Sqp:
 
     for iteration in range(max_iter):
       self._compute_gradients(x_k)
-      print('solve qp for iteration %s' % iteration)
       x_hat, s_hat, pi_hat, minor_iterations = self._solve_qp(x_k)
       x_next, s_next, pi_next, alpha = self._line_search(x_k, s_k, pi_k, x_hat, s_hat, pi_hat)
       self._update_hessian_approximation(x_k, x_next, x_hat, pi_next, alpha)
@@ -62,15 +61,14 @@ class Sqp:
   def _check_convergence(self, x, pi):
     tau_x = self.tol * (1 + max(x))
     tau_pi = self.tol * (1 + max(pi))
+    if any(p < -tau_pi for p in pi): return False
     for i in range(len(self.cs)):
-      if pi[i] < -tau_pi: return False
-
       ci = self.cs[i](x)
-      if ci > tau_x: return False
+      if ci < -tau_x: return False
       if ci * pi[i] > tau_pi: return False
 
       self._compute_gradients(x)
-      d = self.f_grad_k + self.jacobian_k.T * pi
+      d = self.f_grad_k - self.jacobian_k.T * pi
       if any(abs(di) > tau_pi for di in d): return False
     return True
 
@@ -81,18 +79,21 @@ class Sqp:
 
     self._compute_gradients(x1)
     delta = x1 - x0
-    y = self.f_grad_k - f_grad0 + (self.jacobian_k - jacobian0).T * pi1
+    y = self.f_grad_k - f_grad0 - (self.jacobian_k - jacobian0).T * pi1
 
     p = x_hat - x0 # search direction
-    sigma = alpha * (mp.one - self.eta) * p.T * self.hessian_approximation * p
+    sigma = (alpha * (mp.one - self.eta) * p.T * self.hessian_approximation * p)[0]
 
     yTdelta = (y.T * delta)[0]
 
-    if yTdelta >= sigma[0]:
+    if yTdelta >= sigma:
       q = self.hessian_approximation * delta
       qTdelta = (q.T * delta)[0]
       self.hessian_approximation += y * y.T / yTdelta + q * q.T / qTdelta
     elif self.print_stats:
+      print_dps = 10
+      yTdelta = mp.nstr(yTdelta, print_dps)
+      sigma = mp.nstr(sigma, print_dps)
       print("No hessian update. y' delta = %s, sigma = %s" % (yTdelta, sigma))
 
 
@@ -126,12 +127,12 @@ class Sqp:
     
   def _solve_qp(self, x_k):
     """ minimize f_k + g_k'(x - x_k) + 0.5 (x - x_k)' H_k (x - x_k)
-        s.t. c_k + J_k (x - x_k) <= 0
+        s.t. c_k + J_k (x - x_k) >= 0
 
         i.e., 
 
         minimize (g_k - H_k x_k)' x + 0.5 x' H_k x + constants
-        s.t. J_k x <= J_k x_k - c_k
+        s.t. J_k x >= J_k x_k - c_k
     """
     Q = self.hessian_approximation
     
@@ -141,51 +142,82 @@ class Sqp:
     self._compute_gradients(x_k)
     A_ineq = self.jacobian_k
     b_ineq = self.jacobian_k * x_k - self.evaluate_constraints(x_k)
-    if True:
-      print('c')
-      print(self.cs[0](x_k))
-      print('jacobian')
-      print(self.jacobian_k)
-      print('jacobian * x_k')
-      print(self.jacobian_k * x_k)
-      print('jacobian * x_k - constraints')
-      print(self.jacobian_k * x_k - self.evaluate_constraints(x_k))
-      print('Q')
-      print(Q)
-      print('A')
-      print(A_ineq)
-      print('b')
-      print(b_ineq)
-      print('x')
-      print(x_k)
 
-    x, s, pi, f, res, gap, iteration = qp.solve_qp(Q, c, A_eq, b_eq, A_ineq, b_ineq, self.matrix, self.minor_tol)
+    x, s, pi, f, res, gap, iteration = qp.solve_qp(Q, c, A_eq, b_eq, A_ineq, b_ineq, self.matrix, self.minor_tol, 100, False)
     assert res < self.minor_tol, "Res = %s, tol = %s" % (res, self.minor_tol)
     assert gap < self.minor_tol, "Gap = %s, tol = %s" % (gap, self.minor_tol)
-    print('x after')
-    print(x)
     return self.matrix(x), self.matrix(s), self.matrix(pi), iteration
 
+  def _solve_identity_hessian_single_constraint_positive_problem(self, a, b):
+    """
+    minimize x' x
+    subject to a'x = b, x >= 0
+
+    If b >= 0, the solution is 
+    x_i = (b / (ap' ap)) * a_i if a_i > 0, and 0 otherwise,
+    where, ap is the positive elements of a
+    """
+    if b < mp.zero:
+      a, b = -a, -b
+    ap_norm = sum(ap * ap for ap in a if ap > mp.zero)
+    return mp.matrix([max((b / ap_norm) * ai, mp.zero) for ai in a])
+
+  def _update_rho(self, x_k, s_k, pi_k, x_hat, pi_hat):
+    """
+    First find the vector rho* that solves
+    minimize rho' rho
+    subject to phi'(0) = -0.5 p_x' H p_x, rho >= 0,
+
+    where phi(a) is the merit function along the search direction, i.e.,
+
+    phi(a) = f(x + a p_x) - (pi + a p_pi)' (c(x + a p_x) - s - a p_s) + 0.5 sum(rho_i (c_i(x + a p_x) - s_i)^2)
+
+    The derivative of phi at 0 is
+
+    phi'(0) = g' p_x - p_pi' (c - s) - pi' (J p_x - p_s) + sum(rho_i * (c_i - s_i) * (J_i p_x - p_s_i))
+
+    Using J p_x - p_s = -(c - s), we get
+
+    phi'(0) = g' p_x + (pi - p_pi)' (c - s) + sum(rho_i (c_i - s_i)^2),
+
+    which means that the constraint amounts to
+
+    (c - s).^2 rho = g' p_x + (pi - p_pi)' (c - s) - 0.5 p_x' H p_x
+    """
+    self._compute_gradients(x_k)
+    cs = self.evaluate_constraints(x_k)
+    p_x = x_hat - x_k
+    p_pi = pi_hat - pi_k
+    cMinusS = cs - s_k
+    cMinusS2 = self.matrix([c**2 for c in cMinusS])
+    rhs = (self.f_grad_k.T * p_x + (pi_k - p_pi).T * cMinusS - 0.5 * p_x.T * self.hessian_approximation * p_x)[0]
+    if rhs > mp.zero and max(cMinusS2) > mp.zero:
+      rho_star = self._solve_identity_hessian_single_constraint_positive_problem(cMinusS2, rhs)
+      print('New rho: %s' % rho_star)
+      self.rho = rho_star
+
   def _line_search(self, x_k, s_k, pi_k, x_hat, s_hat, pi_hat):
+    self._update_rho(x_k, s_k, pi_k, x_hat, pi_hat)
+
     alpha = mp.one
-    def m(x, s, pi):
+    def merit_function(x, s, pi):
       cs = self.evaluate_constraints(x)
-      phi = self.f(x) + pi.T * (cs - s)
-      for j in range(len(self.cs)):
+      phi = self.f(x) - pi.T * (cs - s)
+      for j in range(len(cs)):
         phi += 0.5 * self.rho[j] * (cs[j] - s[j]) ** 2
       return phi
 
-    m0 = m(x_k, s_k, pi_k)
+    m0 = merit_function(x_k, s_k, pi_k)
     for i in range(30):
       x = (mp.one - alpha) * x_k + alpha * x_hat
       s = (mp.one - alpha) * s_k + alpha * s_hat
       pi = (mp.one - alpha) * pi_k + alpha * pi_hat
-      phi = m(x, s, pi)
+      phi = merit_function(x, s, pi)
       if self.print_stats:
         print_dps = 10
         phi_str = mp.nstr(phi[0], print_dps)
         m0_str = mp.nstr(m0[0], print_dps)
-        print('Value for alpha=%s: %s, m0=%s' % (alpha, phi_str, m0_str))
+        #print('Value for alpha=%s: %s, m0=%s' % (alpha, phi_str, m0_str))
 
       if phi[0] < m0[0]:
         return x, s, pi, alpha
--- tests/test_sqp.py
+++ tests/test_sqp.py
@@ -14,16 +14,23 @@ class TestSQP(unittest.TestCase):
   def test_sqp(self, matrix):
     mp.dps = 100
 
-    f = lambda x: (x[0] - 1)**2 + (x[1] - 0.25)**2
-    c = lambda x: x[0] ** 2 + x[1] ** 2 - mp.one
+    f = lambda x: (x[0] - 1)**2 + (x[1] - 0.25)**2 # (x1-1)^2 + (x2-0.25)^2
+    c = lambda x: -(x[0] ** 2 + x[1] ** 2 - mp.one) # x1^2 + x2^2 <= 1
 
     tol = mp.mpf('1e-50')
     opt = sqp.Sqp(f, None, [c], None)
     x0 = [1, 1]
     x, f, cs = opt.solve(x0)
-    print('f: %s' % f)
-    print('cs: %s' % cs)
-    print('x: %s' % x)
+    print_dps = 10
+    f_str = mp.nstr(f, print_dps)
+    print('f: %s' % f_str)
+    for i in range(len(cs)):
+      cs_str = mp.nstr(cs[i], print_dps)
+      print('c[%s]: %s' % (i, cs_str))
+
+    for i in range(len(x)):
+      x_str = mp.nstr(x[i], print_dps)
+      print('x[%s]: %s' % (i, x_str))
 
 if __name__ == "__main__":
   unittest.main()
