629146aeb5b0a00bae567601de06650ee7bb8fb6
Albin Fredriksson <albin.fredriksson@gmail.com>
2023-12-16 21:16:39 +0100

    Fix test

--- tests/test_sqp.py
+++ tests/test_sqp.py
@@ -11,23 +11,22 @@ import unittest
 from parameterized import parameterized
 
 class TestSQP(unittest.TestCase):
-  @parameterized.expand([dense_mp_matrix.matrix, mp.matrix])
-  def test_sqp(self, matrix):
-    mp.dps = 100
-
-    f = lambda x: (x[0] - 1)**2 + (x[1] - 0.25)**2 # (x1-1)^2 + (x2-0.25)^2
-    c = lambda x: -(x[0] ** 2 + x[1] ** 2 - mp.one) # x1^2 + x2^2 <= 1
-
-    tol = mp.mpf('1e-20')
-    opt = sqp.Sqp(f, None, [c], None, tol=tol, matrix=matrix, print_stats=True)
-    x0 = [1, 1]
+  def _solve_and_print(self, opt, x0, max_iter):
     tic = time.time()
-    x, f, cs, status = opt.solve(x0, 100)
+    x, f, cs, status = opt.solve(x0, max_iter)
     toc = time.time() - tic
+
+    self.assertTrue(status == "Optimal solution found")
+
     print_dps = 10
     print(status)
     f_str = mp.nstr(f, print_dps)
     print('f: %s' % f_str)
+    opt._compute_gradients(x)
+    jac_str = [mp.nstr(c, print_dps) for c in opt._jacobian_k]
+    f_grad_str = [mp.nstr(fi, print_dps) for fi in opt._f_grad_k]
+    print('fgrad: %s' % f_grad_str)
+    print('jac: %s' % jac_str)
     for i in range(len(cs)):
       cs_str = mp.nstr(cs[i], print_dps)
       print('c[%s]: %s' % (i, cs_str))
@@ -36,6 +35,17 @@ class TestSQP(unittest.TestCase):
       x_str = mp.nstr(x[i], print_dps)
       print('x[%s]: %s' % (i, x_str))
     print('Time to solve: %s' % toc)
+
+  @parameterized.expand([dense_mp_matrix.matrix, mp.matrix])
+  def test_sqp(self, matrix):
+    mp.dps = 100
+
+    f = lambda x: (x[0] - 1)**2 + (x[1] - 0.25)**2 # (x1-1)^2 + (x2-0.25)^2
+    c = lambda x: -(x[0] ** 2 + x[1] ** 2 - mp.one) # x1^2 + x2^2 <= 1
+    tol = mp.mpf('1e-20')
+    opt = sqp.Sqp(f, None, [c], None, tol=tol, matrix=matrix, print_stats=True)
+    x0 = [1, 1]
+    self._solve_and_print(opt, x0, 100)
 
   @parameterized.expand([dense_mp_matrix.matrix])
   def test_larger_sqp(self, matrix):
@@ -74,13 +84,12 @@ class TestSQP(unittest.TestCase):
         grad_wrt_dose[i] += 2 * 1 * (dose[i] - mp.zero)
       return dose_matrix_T * grad_wrt_dose
 
-
     def c(x):
       val = mp.zero
       dose = dose_matrix * x
       for i in target:
         val += min(dose[i] - 0.8 * mp.one, mp.zero)**2
-      return -val #-val + mp.mpf('1e-6') # minus sing to get min(d - 0.8, 0)^2 <= 0
+      return -val
 
     def c_grad(x):
       grad_wrt_dose = matrix(n_vox, 1)
@@ -89,32 +98,10 @@ class TestSQP(unittest.TestCase):
         grad_wrt_dose[i] += 2 * min(dose[i] - 0.8 * mp.one, 0)
       return -dose_matrix_T * grad_wrt_dose
         
-    tol = mp.mpf('1e-10')
+    tol = mp.mpf('1e-2')
     opt = sqp.Sqp(f, f_grad, [c], [c_grad], tol=tol, matrix=matrix, print_stats=True)
     x0 = matrix([1] * n_bix)
-    tic = time.time()
-    #print('finite diff: %s' % opt._finite_difference_grad(f, x0))
-    #print('grad: %s' % f_grad(x0))
-
-    x, f, cs, status = opt.solve(x0, 100)
-    toc = time.time() - tic
-    print_dps = 10
-    print(status)
-    f_str = mp.nstr(f, print_dps)
-    print('f: %s' % f_str)
-    opt._compute_gradients(x)
-    jac_str = [mp.nstr(c, print_dps) for c in opt._jacobian_k]
-    f_grad_str = [mp.nstr(fi, print_dps) for fi in opt._f_grad_k]
-    print('fgrad: %s' % f_grad_str)
-    print('jac: %s' % jac_str)
-    for i in range(len(cs)):
-      cs_str = mp.nstr(cs[i], print_dps)
-      print('c[%s]: %s' % (i, cs_str))
-
-    for i in range(len(x)):
-      x_str = mp.nstr(x[i], print_dps)
-      print('x[%s]: %s' % (i, x_str))
-    print('Time to solve: %s' % toc)
+    self._solve_and_print(opt, x0, 100)
 
   def _test_scipy_sqp(self):
     mp.dps = 100
